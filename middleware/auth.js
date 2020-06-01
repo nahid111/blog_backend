b3567f692143828ff2af6cda35b45e19d1e3ff5a
Muhammad Nahid <mdnahid22@gmail.com>
2020-06-01 21:35:08 +0600

    disabled cookie checking in auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -14,10 +14,10 @@ exports.protect = asyncHandler(async (req, res, next) => {
     // settoken from Bearer token in header
     token = req.headers.authorization.split(" ")[1];
   }
-  else if (req.cookies.token) {
-    // set token from cookie
-    token = req.cookies.token;
-  }
+  // else if (req.cookies.token) {
+  //   // set token from cookie
+  //   token = req.cookies.token;
+  // }
 
   // Make sure token exists
   if (!token) {
